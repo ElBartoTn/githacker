0f9be7f2d661d5fdd019ee9396738418731f21ca
sekrafi tarek <tarek.sekrafii89@gmail.com>
2017-11-02 19:04:48 +0100

    update

--- public/js/score.js
+++ public/js/score.js
@@ -1,42 +1,42 @@
 var starsCount = require('./stars.js')
 
-starsCount('devrecipe', function (total) {
+starsCount('christopheranderton', function (total) {
 	console.log(total)
 })
 
 var issuesCount = require('./openIssues.js')
 
-issuesCount('devrecipe', function (total1) {
+issuesCount('christopheranderton', function (total1) {
 	console.log(total1)
 })
 
 var forksCount = require('./forks.js')
 
-forksCount('devrecipe', function (total2) {
+forksCount('christopheranderton', function (total2) {
 	console.log(total2)
 })
 
 var watchersCount = require('./watchers.js')
 
-watchersCount('devrecipe', function (total3) {
+watchersCount('christopheranderton', function (total3) {
 	console.log(total3)
 })
 
 
 var technologielist = require('./technologie.js')
 
-technologielist('devrecipe', function (technologie) {
+technologielist('christopheranderton', function (technologie) {
 	console.log(technologie)
 })
 
 var nbrprojetcount = require('./nbrprojet.js')
 
-nbrprojetcount('devrecipe', function (nbr) {
+nbrprojetcount('christopheranderton', function (nbr) {
 	console.log(nbr)
 })
 
 var getImag = require('./image.js')
 
-getImag('devrecipe', function (image) {
+getImag('christopheranderton', function (image) {
 	console.log(image)
 })
